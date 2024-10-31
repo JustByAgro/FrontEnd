6d13a2812a53f9e15ec4d9185efbbeba2684d379
root <root@sangsung.>
2024-10-31 15:58:51 -0300

    Pagina de login criada e outras alterações
    Adicionado Bootstrap e FontAwesome no index.html e estilização padrão no mesmo.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor (private router: Router) {}
 
+
+  // Funções de animação
+  isPassVisible: boolean = false;
+  changeIcon() {
+    this.isPassVisible = !this.isPassVisible;
+  }
+
+  redirectCadastrar(){
+    this.router.navigate(['/cadastro'])
+  }
 }
