d10a0e75d220d91c606db90ae4ed4ad11b36bff5
root <root@sangsung.>
2024-11-11 11:15:49 -0300

    feat: adiciona classes globais e componentes do comprador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { TelaVendedorComponent } from './components/tela-vendedor/tela-vendedor.component';
+import { BackupComponentsComponent } from './components/backup-components/backup-components.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'', component:LandingPageComponent},
+    {'path':'home',component:HomeComponent},
+    {'path':'vendedor', component:TelaVendedorComponent},
+    {'path':'backup',component:BackupComponentsComponent}
+];
