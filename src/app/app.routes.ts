6a81155d9f5a6067e1dfaeae9963208fdd99ec78
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-11-12 08:16:59 -0300

    wip: rotas de teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { TelaVendedorComponent } from './components/tela-vendedor/tela-vendedor.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { LandingPageComponent } from './paginas/landing-page/landing-page.component';
+import { TelaVendedorComponent } from './paginas/tela-vendedor/tela-vendedor.component';
 import { BackupComponentsComponent } from './components/backup-components/backup-components.component';
+import { CardComponent } from './components/card/card.component';
 
 export const routes: Routes = [
-    {'path':'', component:LandingPageComponent},
-    {'path':'home',component:HomeComponent},
-    {'path':'vendedor', component:TelaVendedorComponent},
-    {'path':'backup',component:BackupComponentsComponent}
+    { path:'', component:LandingPageComponent },
+    { path: 'home', component:HomeComponent },
+    { path: 'vendedor', component:TelaVendedorComponent },
+    /* rotas para teste */
+    { path: 'backup', component:BackupComponentsComponent },
+    { path: 'card', component: CardComponent }
 ];
