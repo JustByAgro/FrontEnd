1b41a9ed43d58a2cb7bc1e264468d9935af6930b
Eduardo Moraes <eduardo2040225@gmail.com>
2024-11-12 18:38:35 -0300

    refactor: tira rota backup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
 import { LandingPageComponent } from './paginas/landing-page/landing-page.component';
 import { TelaVendedorComponent } from './paginas/tela-vendedor/tela-vendedor.component';
-import { BackupComponentsComponent } from './components/backup-components/backup-components.component';
 import { CardComponent } from './components/card/card.component';
 import { LoginComponent } from './paginas/login/login.component';
 import { InputIconComponent } from './components/input-icon/input-icon.component';
@@ -15,7 +14,6 @@ export const routes: Routes = [
     { path: 'vendedor', component:TelaVendedorComponent },
     { path: 'login', component:LoginComponent },
     /* rotas para teste */
-    { path: 'backup', component:BackupComponentsComponent },
     { path: 'card', component:CardComponent },
     { path: 'input', component:InputComponent },
     { path: 'input-icon', component:InputIconComponent },
