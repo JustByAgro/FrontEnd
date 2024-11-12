6d6c4c06e3318dcedf14c7f690a0cb4a88542978
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-11-12 09:11:26 -0300

    fix: tira imports desnecessários

--- src/app/paginas/landing-page/landing-page.component.ts
+++ src/app/paginas/landing-page/landing-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
