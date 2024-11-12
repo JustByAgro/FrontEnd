abb6ca2a08cc2b8ddfddf35e6789f11b720e4cb0
Eduardo Moraes <eduardo.moraes2040@gmail.com>
2024-11-12 17:39:05 -0300

    wip: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,18 @@ import { LandingPageComponent } from './paginas/landing-page/landing-page.compon
 import { TelaVendedorComponent } from './paginas/tela-vendedor/tela-vendedor.component';
 import { BackupComponentsComponent } from './components/backup-components/backup-components.component';
 import { CardComponent } from './components/card/card.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { InputIconComponent } from './components/input-icon/input-icon.component';
+import { InputComponent } from './components/input/input.component';
 
 export const routes: Routes = [
     { path:'', component:LandingPageComponent },
     { path: 'home', component:HomeComponent },
     { path: 'vendedor', component:TelaVendedorComponent },
+    { path: 'login', component:LoginComponent },
     /* rotas para teste */
     { path: 'backup', component:BackupComponentsComponent },
-    { path: 'card', component: CardComponent }
+    { path: 'card', component:CardComponent },
+    { path: 'input', component:InputComponent },
+    { path: 'input-icon', component:InputIconComponent }
 ];
