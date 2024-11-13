08500d0b1e2f9f1682a4ea6c21a76109c4a3581b
root <root@sangsung.>
2024-11-13 15:20:26 -0300

    feat: adiciona navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { BtnComponent } from "../btn/btn.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [BtnComponent, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent { 
+
+
+
+  //Faz a verificação de login
+  isLogado: boolean = false;
+}
