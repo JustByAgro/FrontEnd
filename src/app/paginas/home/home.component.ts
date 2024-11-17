c65abb6658abab455aa1d160d14772208ffe46a1
Fofuxo <mathuythalesu@gmail.com>
2024-11-17 14:40:17 -0300

    feat: concluindo a pagina home

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
