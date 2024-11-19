d8599e1bed229e3959ac961c8471e977f72dfe83
Eduardo Moraes <eduardo2040225@gmail.com>
2024-11-19 16:36:27 -0300

    feat: tela de login

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../components/input/input.component';
+import { InputIconComponent } from '../../components/input-icon/input-icon.component';
+import { BtnComponent } from '../../components/btn/btn.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, InputIconComponent, BtnComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
